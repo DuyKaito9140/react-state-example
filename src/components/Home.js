31d700031fc78c912cb73fc93453d13b73f5b839
DuyKaito9140 <dcduya1nd2018@gmail.com>
2022-03-03 14:48:48 +0700

    main, Lifecycle and Handling events

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,38 @@ import './Home.css'
 export default class Home extends React.Component{
     constructor(props){
         super(props);
-        this.state = {bootcamp: 'Rookies'}
+        this.state = {bootcamp: 'Rookies', time: new Date(), clickedTime: 0}
     }
-
-    render(){
+    render(){    
+        setInterval(()=>{
+            this.setState({
+                time: new Date(),
+            });
+        }, 1000);        
+        const clickGame = ()=>{
+            this.setState({
+                clickedTime: this.state.clickedTime++,
+            });
+            console.log(this.state.clickedTime);
+            if(this.state.clickedTime % 3 == 0){
+                alert("Hello! I am an alert box!! FIZZ");
+            }
+            else if(this.state.clickedTime % 5 == 0){
+                alert("Hello! I am an alert box!! BUZZ");
+            }
+            else if(this.state.clickedTime % 5 == 0 && this.state.clickedTime % 3 == 0){
+                alert("Hello! I am an alert box!! FIZZ && BUZZ");
+            }
+            else{
+                alert("Next Click");
+            }
+          }
         return(
-            <React.Fragment>
+            <React.Fragment>    
                 <h2>Welcome to {this.state.bootcamp}</h2>
-            </React.Fragment>
+                <h2>It is: {this.state.time.toLocaleTimeString()}</h2>
+                <button id="btnbtn" onClick={()=>clickGame()}>Clicky Game </button>                                
+            </React.Fragment>            
         );
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import './Home.css'
 export default class Navbar extends React.Component{
     constructor(props){
         super(props);
-        this.state = {username: 'DƯƠNG CÔNG DUY'}
+        this.state = {username: 'DUY'}
     }
 
     render(){
@@ -12,14 +12,15 @@ export default class Navbar extends React.Component{
             <React.Fragment>
                  <nav id='navbar'>
                     <ul>
-                    <a href="#"><li>Home</li></a>
-                    <a href="#"><li>Contact</li></a>
-                    <a href="#"><li>About</li></a>
+                        <a href="#"><li>Home</li></a>
+                        <a href="#"><li>Contact</li></a>
+                        <a href="#"><li>About</li></a>
                     </ul>
+                    <div className="nav-details"> 
+                        <p className = "nav-username" >{this.state.username}</p> 
+                    </div> 
                  </nav>
-                 <div className="nav-details"> 
-                    <p className = "nav-username" >{this.state.username}</p> 
-                </div> 
+                 
             </React.Fragment>
         );
     }
